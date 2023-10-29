2188748e115c2c1198eabaadacaf55414e23b8a9
yura-prots <yuraprots@gmail.com>
2023-10-29 17:14:11 +0200

    додав і змінив modal.js

---
+++ js/modal.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('.data-modal-open'),
+    closeModalBtn: document.querySelector('.data-modal-close'),
+    backdrop: document.querySelector('.backdrop'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.backdrop.classList.toggle('is-hidden');
+    console.log('clicked');
+  }
+})();
